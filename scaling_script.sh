73bb6ebb90f7cdc376cbc8056b6ef1c8bb5e889e
denocris <denobili16@gmail.com>
2017-04-26 19:35:38 +0200

    scaling ulysses

--- scaling_script.sh
+++ scaling_script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#PBS -l walltime=00:30:00
+#PBS -l walltime=00:15:00
 #PBS -l nodes=1:ppn=20
 
 module load openmpi/1.8.3/gnu/4.9.2
 
-cd $PBS_O_WORKDIR
+cd P2.10_seed/
+rm -f timing.dat
+rm -f timing_plot.dat
 
 for numprocs in 1 2 4 8 16 20
 do
