21b74f90f3f48a40704bbdc3d8e8685b275f9b91
denocris <denobili16@gmail.com>
2017-04-27 10:09:12 +0200

    mod script

--- scaling_script.sh
+++ scaling_script.sh
@@ -6,12 +6,12 @@
 module load openmpi/1.8.3/gnu/4.9.2
 
 cd P2.10_seed/input
-rm -f timing.dat
-#rm -f timing_plot.dat
+rm -f ../data/timing_A.dat
+rm -f ../data/timing_A_clean.dat
 
-for nprocs in 1 2 4 8 16;
+for nprocs in 1 2 4 8 16 20;
 do
-    /usr/bin/time -p mpirun -np $nprocs ../src/simplemd.x in 2>> timing.dat
+    /usr/bin/time -p mpirun -np $nprocs ../src/simplemd.x in >> ../data/timing_A.dat
 done
 
-cat timing.dat | grep real | awk '{print $2}' > timing_plot.dat
+cat ../timing_A.dat | grep real | awk '{print $2}' > timing_A_clean.dat
